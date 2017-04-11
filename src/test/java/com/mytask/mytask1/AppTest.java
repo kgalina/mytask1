902bf5d39b6997d1ede1d487cd83a109a2adc9cd
kgalina <Galyna.Kuleshova@ligazakon.ua>
2017-04-11 13:22:36 +0300

    Thread.sleep(4000); was deleted

--- src/test/java/com/mytask/mytask1/AppTest.java
+++ src/test/java/com/mytask/mytask1/AppTest.java
@@ -56,4 +56,33 @@ public class AppTest {
       driver.close();
       }
 
+    @Test
+    /* Шаги: 1. Зайти на страницу логина.
+    2. Не заполняя поля email и password нажать "Увійти"
+    ОР: Сообщения "Поле ... має бути заповнене"
+    */
+    public void test_login_empty_fields() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "E:/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.get("https://webep-dev.ligazakon.net");
+
+        WebElement sign_in_btn = driver.findElement(By.xpath("//a[contains(text(),'Увійти')]"));
+        sign_in_btn.click();
+
+        Thread.sleep(2000);
+
+        WebElement login_btn = driver.findElement(By.xpath("//button[contains(text(),'Увійти')]"));
+        login_btn.submit();
+
+        Thread.sleep(1000);
+        WebElement empty_email_message = driver.findElement(By.xpath("//span[contains(@ng-show, 'sign_in.email.$error.required')]"));
+        Assert.assertEquals("Пустое поле e-mail", "Поле e-mail має бути заповнене", empty_email_message.getText());
+
+        WebElement empty_password_message = driver.findElement(By.xpath("//span[contains(@ng-show, 'sign_in.password.$error.required')]"));
+        Assert.assertEquals("Пустое поле Пароль", "Поле Пароль має бути заповнене", empty_password_message.getText());
+
+        driver.close();
+    }
 }
