dd37e0e3cf6c1bb687dee5e8c3af420841200456
kgalina <Galyna.Kuleshova@ligazakon.ua>
2017-04-11 16:50:23 +0300

    add check email and user_name

--- src/test/java/com/mytask/mytask1/Login.java
+++ src/test/java/com/mytask/mytask1/Login.java
@@ -1,11 +1,13 @@
 package com.mytask.mytask1;
 
+import org.apache.http.util.Asserts;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 public class Login {
@@ -43,12 +45,14 @@ public class Login {
     /* Шаги: 1. Перейти по URL на страницу логина.
     2. Ввести логин и пароль.
     3. Нажать "Увійти".
-    ОР: Сообщения "Поле ... має бути заповнене"
+    4. Нажать кнопку "Мой профиль"
+    ОР: Отображатеся email, имя пользователя
     */
     public void test_login_successful() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "E:/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+//        System.setProperty("webdriver.chrome.driver", "E:/chromedriver.exe");
+//        WebDriver driver = new ChromeDriver();
 
+        WebDriver driver = new FirefoxDriver();
         driver.manage().window().maximize();
         driver.get("https://webep-dev.ligazakon.net/signinup/auth");
 
@@ -64,5 +68,18 @@ public class Login {
 
         WebElement login_btn = driver.findElement(By.xpath("//button[contains(text(),'Увійти')]"));
         login_btn.click();
+
+        Thread.sleep(2000);
+
+        WebElement profile_btn = driver.findElement(By.id("profile-btn"));
+        profile_btn.click();
+
+        WebElement profile_email = driver.findElement(By.xpath("//h5[contains(@class, 'lz-user-email')][1]"));
+        Assert.assertEquals("email в профиле", "galyna.kuleshova@ligazakon.ua", profile_email.getText() );
+
+        WebElement user_name = driver.findElement(By.xpath("//*[@id='profMenu']//*[contains(@class, 'lz-user-name')]"));
+        Assert.assertEquals("Имя, Фамилия в профиле", "Gala Kuleshova" , user_name.getText());
+
+        driver.close();
     }
 }
