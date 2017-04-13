652f6862c20d134b1f7c1179176ff3494d7f844d
kgalina <Galyna.Kuleshova@ligazakon.ua>
2017-04-13 10:36:22 +0300

    add @BeforeClass @AfterClass in NavigationOpenLevel tests

--- src/test/java/com/mytask/mytask1/NavigationOpenLevel.java
+++ src/test/java/com/mytask/mytask1/NavigationOpenLevel.java
@@ -1,6 +1,8 @@
 package com.mytask.mytask1;
 
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -8,36 +10,32 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
-public class AppTest {
-    //Open уровень
+public class NavigationOpenLevel {
+    private static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp() {
+        System.setProperty("webdriver.chrome.driver", "E:/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://webep-dev.ligazakon.net");
+    }
 
     @Test
     public void test_help_page_title() throws InterruptedException {
-//		 System.setProperty("webdriver.gecko.driver", "D:/Projects/geckodriver-v0.15.0-win32/geckodriver.exe");
+//		System.setProperty("webdriver.gecko.driver", "D:/Projects/geckodriver-v0.15.0-win32/geckodriver.exe");
 //      WebDriver driver = new FirefoxDriver();
 
 //      Проверка тайтла страницы "Помощь"
-        System.setProperty("webdriver.chrome.driver", "E:/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-
-        driver.manage().window().maximize();
-        driver.get("https://webep-dev.ligazakon.net");
-
         WebElement help_icon = driver.findElement(By.xpath("//div[@class='lz-ico']"));
         help_icon.click();
 
         String title = driver.getTitle();
         Assert.assertEquals("Title Про продукт", "Про продукт", title);
-        driver.close();
-    }
+     }
 
     @Test
     public void test_level_up() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "E:/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-
-        driver.manage().window().maximize();
-        driver.get("https://webep-dev.ligazakon.net");
 
         WebElement help_icon = driver.findElement(By.xpath("//div[@class='lz-ico']"));
         help_icon.click();
@@ -53,8 +51,10 @@ public class AppTest {
         WebElement search_title_element = driver.findElement(By.xpath("//div[text()='Пошук']"));
         String search_text = search_title_element.getText();
         Assert.assertEquals("Текст Пошук", "Пошук", search_text);
-
-        driver.close();
     }
 
+    @AfterClass
+    public static void tearDown() {
+        driver.close();
+    }
 }
