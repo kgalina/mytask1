6a57cd6b8356353966543e1ece591b4a51c960a7
kgalina <Galyna.Kuleshova@ligazakon.ua>
2017-05-15 17:57:38 +0300

    add AuthPage class

---
+++ src/test/java/com/mytask/mytask1/AuthPage.java
@@ -0,0 +1,19 @@
+package com.mytask.mytask1;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AuthPage {
+
+
+    @FindBy (xpath="(//input[@id='email'])[3]")
+    public WebElement email_element ;
+
+    @FindBy(xpath = "//button[contains(text(),'Увійти')]")
+    public WebElement login_btn_element;
+
+    public void UserClicks(WebElement element){
+        element.click();
+    }
+}
+
