9d13313a76921e8e539c42e8615c68ae9d68dccd
kgalina <Galyna.Kuleshova@ligazakon.ua>
2017-05-16 17:18:24 +0300

    move xpaths from test to page

--- src/test/java/com/mytask/mytask1/IncorrectAuthorizationMessagesPageObj.java
+++ src/test/java/com/mytask/mytask1/IncorrectAuthorizationMessagesPageObj.java
@@ -1,7 +1,6 @@
 package com.mytask.mytask1;
 
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -10,10 +9,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import static com.mytask.mytask1.Constants.URL;
+import static org.junit.Assert.assertEquals;
 
 public class IncorrectAuthorizationMessagesPageObj {
     private static WebDriver driver;
-    private static WebElement email;
     private static WebElement password;
     private static AuthPage authPage;
     @BeforeClass
@@ -29,8 +28,6 @@ public class IncorrectAuthorizationMessagesPageObj {
         WebElement sign_in_btn = driver.findElement(By.xpath("//a[contains(text(),'Увійти')]"));
         sign_in_btn.click();
         Thread.sleep(2000);
-        email = driver.findElement(By.xpath("(//input[@id='email'])[3]"));
-        password = driver.findElement(By.id("password"));
         authPage.clickElement(authPage.login_btn);
     }
 
@@ -38,15 +35,12 @@ public class IncorrectAuthorizationMessagesPageObj {
     /* 2. Не заполняя поля email и password нажать "Увійти"
     ОР: Сообщения "Поле ... має бути заповнене" */
     public void testAuthEmptyFields() throws InterruptedException {
-        email.clear();
-        password.clear();
+        authPage.email_input.clear();
+        authPage.passwd_input.clear();
 
         Thread.sleep(1000);
-        WebElement empty_email_message = driver.findElement(By.xpath("//span[contains(@ng-show, 'sign_in.email.$error.required')]"));
-        Assert.assertEquals("Сообщение, если не заполнено e-mail (укр.)", "Поле e-mail має бути заповнене", empty_email_message.getText());
-
-        WebElement empty_password_message = driver.findElement(By.xpath("//span[contains(@ng-show, 'sign_in.password.$error.required')]"));
-        Assert.assertEquals("Сообщение, если не заполнен пароль", "Поле Пароль має бути заповнене", empty_password_message.getText());
+        assertEquals("Сообщение, если не заполнено e-mail (укр.)", "Поле e-mail має бути заповнене", authPage.empty_email_message.getText());
+        assertEquals("Сообщение, если не заполнен пароль", "Поле Пароль має бути заповнене", authPage.empty_passwd_message.getText());
     }
 
     @AfterClass
