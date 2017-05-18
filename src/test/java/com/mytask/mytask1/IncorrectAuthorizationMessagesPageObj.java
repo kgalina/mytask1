3ae491410b3e6a2ce2ee36e91c746caaa6c5bc17
kgalina <Galyna.Kuleshova@ligazakon.ua>
2017-05-18 17:48:17 +0300

    add MainPltPage

--- src/test/java/com/mytask/mytask1/IncorrectAuthorizationMessagesPageObj.java
+++ src/test/java/com/mytask/mytask1/IncorrectAuthorizationMessagesPageObj.java
@@ -1,12 +1,9 @@
 package com.mytask.mytask1;
 
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import static com.mytask.mytask1.Constants.URL;
@@ -15,6 +12,7 @@ import static org.junit.Assert.assertEquals;
 public class IncorrectAuthorizationMessagesPageObj {
     private static WebDriver driver;
     private static AuthPage authPage;
+    private static MainPltPage mainPltPage;
     @BeforeClass
     public static void setUp() throws InterruptedException {
     /* Шаги: 1. Зайти на страницу авторизации через кнопку "Увійти" главной стр. платформы*/
@@ -24,11 +22,10 @@ public class IncorrectAuthorizationMessagesPageObj {
         driver.get(URL.DEV);
 
         authPage = new AuthPage(driver);
+        mainPltPage = new MainPltPage(driver);
 
-        WebElement sign_in_btn = driver.findElement(By.xpath("//a[contains(text(),'Увійти')]"));
-        sign_in_btn.click();
+        mainPltPage.sign_in_btn.click();
         Thread.sleep(2000);
-        authPage.clickElement(authPage.login_btn);
     }
     @Test
     /* 2. Не заполняя поля email и password нажать "Увійти"
@@ -36,6 +33,7 @@ public class IncorrectAuthorizationMessagesPageObj {
     public void testAuthEmptyFields() throws InterruptedException {
         authPage.email_input.clear();
         authPage.passwd_input.clear();
+        authPage.clickElement(authPage.login_btn);
         assertEquals("Сообщение, если не заполнено e-mail (укр.)", "Поле e-mail має бути заповнене", authPage.empty_email_message.getText());
         assertEquals("Сообщение, если не заполнен пароль", "Поле Пароль має бути заповнене", authPage.empty_passwd_message.getText());
     }
