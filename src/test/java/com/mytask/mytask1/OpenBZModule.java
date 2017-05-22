97e72913b1b668752f57e33677435c64155485dd
kgalina <Galyna.Kuleshova@ligazakon.ua>
2017-05-22 12:11:29 +0300

    wait and pageObject

--- src/test/java/com/mytask/mytask1/OpenBZModule.java
+++ src/test/java/com/mytask/mytask1/OpenBZModule.java
@@ -1,18 +1,16 @@
 package com.mytask.mytask1;
 
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static com.mytask.mytask1.Constants.URL;
+import static org.junit.Assert.assertEquals;
 
 public class OpenBZModule {
     private static WebDriver driver;
@@ -20,6 +18,7 @@ public class OpenBZModule {
     private static MainPltPage mainPltPage;
     private static MainIpsPage mainIpsPage;
     private static AccountantAndLawPage accountantAndLawPage;
+    private static Wait<WebDriver> wait;
     @BeforeClass
     public static void setUp() throws InterruptedException {
         /*Шаги: 1. Залогиниться
@@ -30,7 +29,7 @@ public class OpenBZModule {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(URL.AUTH_DEV);
-        Wait<WebDriver> wait = new WebDriverWait(driver, 5).withMessage("Элемент не найден");
+        wait = new WebDriverWait(driver, 5, 1000).withMessage("Элемент не найден");
         authPage = new AuthPage(driver);
         mainPltPage = new MainPltPage(driver);
         mainIpsPage = new MainIpsPage(driver);
@@ -40,7 +39,7 @@ public class OpenBZModule {
         authPage.email_input.sendKeys("Galyna.Kuleshova@ligazakon.ua");
 
         authPage.passwd_input.clear();
-        authPage.passwd_input.sendKeys("**");
+        authPage.passwd_input.sendKeys("*");
 
         authPage.clickAndWaitNextElement(authPage.login_btn, mainPltPage.grand_product);
         mainPltPage.grand_product.click();
@@ -50,18 +49,18 @@ public class OpenBZModule {
         }
 // Perform the actions on new window
         mainIpsPage.clickAndWaitNextElement(mainIpsPage.acc_and_law_box, accountantAndLawPage.acc_and_law_header);
-//        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//h1[text()='БУХГАЛТЕР И ЗАКОН']"))));
     }
     @Test
     public void testBZModuleName() throws InterruptedException {
         /* Проверка названия открытого модуля "БУХГАЛТЕР И ЗАКОН"*/
-       Assert.assertTrue("Название модуля'Бухгалтер и закон' не соответсвует ожидаемому", !driver.findElements(By.xpath("//h1[text()='БУХГАЛТЕР И ЗАКОН']")).isEmpty());
+        wait.until(ExpectedConditions.visibilityOf(accountantAndLawPage.acc_and_law_header));
+        assertEquals("Название модуля'Бухгалтер и закон' не соответсвует ожидаемому", "БУХГАЛТЕР И ЗАКОН" , accountantAndLawPage.acc_and_law_header.getText());
     }
     @Test
     public void testBZModuleTitle() throws InterruptedException {
         /* Проверка тайтла модуля "БУХГАЛТЕР И ЗАКОН"*/
         String bz_module_title = driver.getTitle();
-        Assert.assertEquals("Тайтл модуля 'Бухгалтер и Закон' не соответствует ожидаемому",bz_module_title, "БУХГАЛТЕР И ЗАКОН");
+        assertEquals("Тайтл модуля 'Бухгалтер и Закон' не соответствует ожидаемому","БУХГАЛТЕР И ЗАКОН", bz_module_title);
     }
     @AfterClass
     public static void tearDown() {
